e80827203255711860923ce026a66671e2644c4c
Venkat <venkatganesh.1596@gmail.com>
2020-11-20 12:44:34 -0800

    Updated script to send out mails

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -13,9 +16,18 @@ type jobsearch struct {
 	Location string
 }
 
+type email struct {
+	EmailPassword string
+	From          string
+	TO            string
+	Subject       string
+	Body          string
+}
+
 type Config struct {
 	Jobsearchs      []jobsearch
 	OutputDirectory string
+	Email           email
 }
 
 func LoadConfiguration() Config {
@@ -31,6 +43,7 @@ func LoadConfiguration() Config {
 	}
 	viper.UnmarshalKey("jobsearches", &config.Jobsearchs)
 	viper.UnmarshalKey("outputFilePath", &config.OutputDirectory)
+	viper.UnmarshalKey("email", &config.Email)
 	return config
 }
 
@@ -47,6 +60,79 @@ func ExtractJob(position string, localtion string, outputPath string) {
 
 }
 
+func addFiles(w *zip.Writer, basePath, baseInZip string) {
+	// Open the Directory
+	files, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(basePath + file.Name())
+		if !file.IsDir() {
+			dat, err := ioutil.ReadFile(basePath + file.Name())
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			// Add some files to the archive.
+			f, err := w.Create(baseInZip + file.Name())
+			if err != nil {
+				fmt.Println(err)
+			}
+			_, err = f.Write(dat)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else if file.IsDir() {
+
+			// Recurse
+			newBase := basePath + file.Name() + "/"
+			fmt.Println("Recursing and Adding SubDir: " + file.Name())
+			fmt.Println("Recursing and Adding SubDir: " + newBase)
+
+			addFiles(w, newBase, baseInZip+file.Name()+"/")
+		}
+	}
+}
+
+func ZipWriter(source string, destination string) {
+	baseFolder := source
+
+	// Get a Buffer to Write To
+	outFile, err := os.Create(destination)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer outFile.Close()
+
+	// Create a new zip archive.
+	w := zip.NewWriter(outFile)
+
+	// Add some files to the archive.
+	addFiles(w, baseFolder, "")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Make sure to check the error on Close.
+	err = w.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func sendEmails(config Config) {
+	os.MkdirAll(config.OutputDirectory, os.ModePerm)
+	ZipWriter(config.OutputDirectory, "./jobs.zip")
+	cmd := exec.Command("python3", "sendEmails.py", config.Email.From, config.Email.EmailPassword, config.Email.TO, "./jobs.zip")
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Sent job listing email")
+}
+
 func main() {
 	c := LoadConfiguration()
 	// parallel processing
@@ -56,5 +142,6 @@ func main() {
 	}
 
 	fmt.Printf("Finished job parsing")
+	sendEmails(c)
 
 }
---
+++ sendEmails.py
@@ -0,0 +1,62 @@
+import smtplib
+from pathlib import Path
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+from email import encoders
+import sys
+
+
+def send_mail(send_from, send_to, subject, message, files=[],
+              server="localhost", port=587, username='', password='',
+              use_tls=True):
+    """Compose and send email with provided info and attachments.
+
+    Args:
+        send_from (str): from name
+        send_to (list[str]): to name(s)
+        subject (str): message title
+        message (str): message body
+        files (list[str]): list of file paths to be attached to email
+        server (str): mail server host name
+        port (int): port number
+        username (str): server auth username
+        password (str): server auth password
+        use_tls (bool): use TLS mode
+    """
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(message))
+
+    for path in files:
+        part = MIMEBase('application', "octet-stream")
+        with open(path, 'rb') as file:
+            part.set_payload(file.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition',
+                        'attachment; filename="{}"'.format(Path(path).name))
+        msg.attach(part)
+
+    smtp = smtplib.SMTP(server, port)
+    if use_tls:
+        smtp.starttls()
+    smtp.login(username, password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
+
+if __name__ == "__main__":
+    FROM_ADDRESS = "venkatganesh.1596@gmail.com"
+    EMAIL_ADDRESS = sys.argv[1]
+    EMAIL_PASSWORD = sys.argv[2]
+    TO_ADDRESS = sys.argv[3]
+    FILES = [sys.argv[4]]
+    SUBJECT = "Apply for todays job listing"
+    BODY = "Hi, \n Checkout the attached file for job listing and hype links"
+
+    send_mail(FROM_ADDRESS, TO_ADDRESS, SUBJECT, BODY, FILES, "smtp.gmail.com", 587, EMAIL_ADDRESS, EMAIL_PASSWORD)
+
